728b7d0443696f7ca2cb3638b20668648af7787f
CHENNULUSUGITHI <chennulusugithi786@gmail.com>
2023-07-10 14:29:58 +0530

    first commit

---
+++ app/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+
+def validate_for_a(Svalue):
+    if Svalue[0].lower()=='a':
+        raise forms.ValidationError("first letter should not be a")
+
+
+def validate_for_len(name):
+    if len(name)<=5:
+        raise forms.ValidationError('len is less than 5')
+
+
+class StudentForm(forms.Form):
+    Sname=forms.CharField(max_length=100,validators=[validate_for_a])
+    Sage=forms.IntegerField()
+
